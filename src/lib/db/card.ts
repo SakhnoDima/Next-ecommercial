73c418feb104bdf61d4e546e9db8828aed6af1d2
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2024-02-14 17:35:42 +0200

    ficha: set user cart and anonimus cart

--- src/lib/db/card.ts
+++ src/lib/db/card.ts
@@ -1,6 +1,8 @@
 import { cookies } from "next/headers";
 import { prisma } from "./prisma";
-import { Prisma } from "@prisma/client";
+import { Card, Prisma } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export type CardWithProducts = Prisma.CardGetPayload<{
   include: { items: { include: { product: true } } };
@@ -16,11 +18,22 @@ export type TShopingCard = CardWithProducts & {
 };
 
 export const createCard = async (): Promise<TShopingCard> => {
-  const newCard = await prisma.card.create({
-    data: {},
-  });
+  const session = await getServerSession(authOptions);
+  let newCard: Card;
 
-  cookies().set("localCard", newCard.id);
+  if (session) {
+    newCard = await prisma.card.create({
+      data: {
+        userId: session.user.id,
+      },
+    });
+  } else {
+    newCard = await prisma.card.create({
+      data: {},
+    });
+    //! Note: needs encrypt and secure in real product app!
+    cookies().set("localCard", newCard.id);
+  }
 
   return {
     ...newCard,
@@ -31,14 +44,26 @@ export const createCard = async (): Promise<TShopingCard> => {
 };
 
 export const getCard = async (): Promise<TShopingCard | null> => {
-  const localCard = cookies().get("localCard")?.value;
-  if (!localCard) {
-    return null;
+  const session = await getServerSession(authOptions);
+  let card: CardWithProducts | null = null;
+
+  if (session) {
+    card = await prisma.card.findFirst({
+      where: {
+        userId: session.user.id,
+      },
+      include: { items: { include: { product: true } } },
+    });
+  } else {
+    const localCard = cookies().get("localCard")?.value;
+    if (!localCard) {
+      return null;
+    }
+    card = await prisma.card.findUnique({
+      where: { id: localCard },
+      include: { items: { include: { product: true } } },
+    });
   }
-  const card = await prisma.card.findUnique({
-    where: { id: localCard },
-    include: { items: { include: { product: true } } },
-  });
 
   if (!card) {
     return null;
